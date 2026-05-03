b0dc6633a7b75e05b0ad809698a0d84c5103ffac
dzenthai <dzenthai@proton.me>
2026-05-03 23:55:12 +0500

    add: update method (data\movies.go);

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -80,7 +80,21 @@ func (m MovieModel) Get(id int64) (*Movie, error) {
 }
 
 func (m MovieModel) Update(movie *Movie) error {
-	return nil
+	query := `
+		UPDATE movies
+		SET title = $1, year = $2, runtime = $3, genres = $4, version = version + 1
+		WHERE id = $5
+		RETURNING version`
+
+	args := []any{
+		movie.Title,
+		movie.Year,
+		movie.Runtime,
+		pq.Array(movie.Genres),
+		movie.ID,
+	}
+
+	return m.db.QueryRow(query, args...).Scan(&movie.Version)
 }
 
 func (m Movie) Delete(id int64) error {
