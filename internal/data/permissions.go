6dd14fb83871fe91d081fd36ee7e274da46892cb
dzenthai <dzenthai@proton.me>
2026-05-29 20:48:10 +0500

    update: adding user permissions during registration;

--- internal/data/permissions.go
+++ internal/data/permissions.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"database/sql"
 	"time"
+
+	"github.com/lib/pq"
 )
 
 type Permissions []string
@@ -54,4 +56,16 @@ func (m PermissionModel) GetAllForUser(userID int64) (Permissions, error) {
 	}
 
 	return permissions, nil
+}
+
+func (m PermissionModel) AddForUser(userID int64, codes ...string) error {
+	query := `
+		INSERT INTO users_permissions
+		SELECT $1, permissions.id FROM permissions WHERE permissions.code = ANY($2)`
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+	defer cancel()
+
+	_, err := m.db.ExecContext(ctx, query, userID, pq.Array(codes))
+	return err
 }
